1f8b59395ddf91c5cfb7b19095f57201f894a1e8
ilkaydnc <ilkaydinc1@gmail.com>
2020-09-14 17:32:36 +0300

    Add getRandomMutlipleOf10 function to particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,4 +1,10 @@
 const PARTICLES = [];
+
+// Get numbers from 50 to 100 that are multiples of 10
+const getRandomMutlipleOf10 = () => {
+  return Math.max(50, Math.min(100, getRandom(10, 5) * 10));
+};
+
 // Get random numbers
 const getRandom = (last, first = 0) => {
   return Math.floor(Math.random() * last) + first;
