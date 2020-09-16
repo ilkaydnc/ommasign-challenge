3a1078232ba235228e84f78b16377de02973262b
ilkaydnc <ilkaydinc1@gmail.com>
2020-09-16 13:29:06 +0300

    Create config.js file

---
+++ config.js
@@ -0,0 +1,26 @@
+export default {
+  menu: [
+    {
+      title: "Barley Salad",
+      img: "./assets/plate1.png",
+      price: "$12",
+    },
+    {
+      title: "Green Apple Salad",
+      img: "./assets/plate2.png",
+      price: "$20",
+    },
+    {
+      title: "Quinoa Salad",
+      img: "./assets/plate3.png",
+      price: "18$",
+    },
+    {
+      title: "Green Bean Soap",
+      img: "./assets/plate4.png",
+      price: "$15",
+    },
+  ],
+  // Recommended creating a maximum of 100 to avoid performance problems.
+  particles: 100,
+};
